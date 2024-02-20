1fbc89c465d8702892945c83595f9112caf35476
aequanimitas <htavenido@gmail.com>
2024-02-20 16:21:41 +0800

    first commit

---
+++ lib/ex2.ex
@@ -0,0 +1,22 @@
+defmodule Ex2 do
+  @moduledoc """
+  Documentation for `Ex2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ex2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def hi do
+    :hello
+  end
+end
---
+++ test/ex2_test.exs
@@ -0,0 +1,8 @@
+defmodule Ex2Test do
+  use ExUnit.Case
+  doctest Ex2
+
+  test "greets the world" do
+    assert Ex2.hello() == :world
+  end
+end
